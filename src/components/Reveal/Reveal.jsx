1ec16ebb304733bcc21611dd6e14c1dbcc29c0ef
complianceX <gandratecnologia@outlook.com.br>
2026-06-28 09:47:20 -0400

    0003

--- src/components/Reveal/Reveal.jsx
+++ src/components/Reveal/Reveal.jsx
@@ -25,8 +25,8 @@ export default function Reveal({ children, delay = 0, className = '' }) {
   return (
     <div
       ref={ref}
-      className={`transition-all duration-700 ease-out will-change-transform ${
-        visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+      className={`transition-all duration-[800ms] ease-out will-change-transform ${
+        visible ? 'opacity-100 translate-y-0 scale-100' : 'opacity-0 translate-y-12 scale-[0.96]'
       } ${className}`}
       style={{ transitionDelay: visible ? `${delay}ms` : '0ms' }}
     >
